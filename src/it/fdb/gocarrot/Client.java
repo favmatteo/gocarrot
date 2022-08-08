0a6fc20cc79a9df68981761e0d52721b143b939d
Matteo <93406671+favmatteo@users.noreply.github.com>
2022-08-09 00:14:56 +0200

    fix IP
    IP portato a come descritto nel file readme

--- src/it/fdb/gocarrot/Client.java
+++ src/it/fdb/gocarrot/Client.java
@@ -17,7 +17,7 @@ public class Client {
 
     public Client(Board board){
         try {
-            socket = new Socket("gocarrot.andybonny.org", 5050);
+            socket = new Socket("127.0.0.1", 5050);
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataInputStream = new DataInputStream(socket.getInputStream());
             this.board = board;
